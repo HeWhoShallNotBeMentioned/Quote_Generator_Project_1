5758448bafdd93b4922f86f470bc5d4c7204e3ec
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-02 14:41:42 -0700

    added comments. changed parameter name in printQuote

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//data for the application stored as an array of objects.
 var quotes = [
   {quote: "To err is human but to really foul things up requires a computer.", source: "Anonymous", citation: "Farmers Almanac", year: "1978", tags: "Technology"},
   {quote: "I've been things and seen places.", source: "Mae West", citation: "I'm No Angel", year: "1933", tags: "Virtue"},
@@ -6,13 +7,16 @@ var quotes = [
 
 ];
 
+//function randomly determines which quote object will be chosen and returns it.
 function getRandomQuote (quotes) {
   var ind = Math.floor(Math.random() * quotes.length);
   var chosenQuote = quotes[ind];
   return chosenQuote;
 }
 
-function printQuote(htmlQuote) {
+//requests quote from getRandomQuote. Processes that quote into the html that will be displayed.
+//Uses conditionals to print out the optional properties only when they are present.
+function printQuote(chosenQuote) {
   var quoteObj = getRandomQuote(quotes);
   var htmlQuote = '<p class="quote">' + quoteObj.quote + '</p>';
   htmlQuote += '<p class="source">' + quoteObj.source;
@@ -26,10 +30,11 @@ function printQuote(htmlQuote) {
     htmlQuote += '<span class="year">' + quoteObj.tags + '</span>';
   }
   htmlQuote += '</p>';
-  // event listener to respond to "Show another quote" button clicks
-  // when user clicks anywhere on the button, the "printQuote" function is called
+
+//prints the code on the page.
   document.getElementById('quote-box').innerHTML = htmlQuote;
 
 }
-
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
