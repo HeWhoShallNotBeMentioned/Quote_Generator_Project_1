f8298b43f018d7c25728e68e8b183f006f3c8158
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-02 16:23:42 -0700

    Add comments and a quote

--- js/script.js
+++ js/script.js
@@ -3,19 +3,21 @@ var quotes = [
   {quote: "To err is human but to really foul things up requires a computer.", source: "Anonymous", citation: "Farmers Almanac", year: "1978", tags: "Technology"},
   {quote: "I've been things and seen places.", source: "Mae West", citation: "I'm No Angel", year: "1933", tags: "Virtue"},
   {quote: "Marriage isn't a word...it's a sentence!", source: "King Vidor", citation: "The Crowd", year: "1928", tags: "Marriage"},
+  {quote: "Doh!", source: "Homer Simpson", citation: "", year: "", tags: "Surprise"}
   //{quote: "", source: "", citation: "", year: "", tags: ""},
-
 ];
 
+//data for the application stored as an array.
 var colors = ["blue","red", "silver", "blueviolet", "darkgreen", "silver", "steeleblue", "sienna", "plum", "magenta", "lightseagreen"];
 
-//function randomly determines which quote object will be chosen and returns it.
+//function randomly determines which quote object will be chosen from quotes and returns it.
 function getRandomQuote (quotes) {
   var ind = Math.floor(Math.random() * quotes.length);
   var chosenQuote = quotes[ind];
   return chosenQuote;
 }
 
+//function randomly determines which color will be chosen from colors and returns it.
 function getRandomColor(colors) {
   var ind = Math.floor(Math.random() * colors.length);
   var chosenColor = colors[ind];
