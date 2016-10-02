55f51a7f528ee4ce21ddbc66b4300a02518d6006
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-01 22:42:08 -0700

    Basic quote functionality implemented

--- js/script.js
+++ js/script.js
@@ -6,23 +6,23 @@ var quotes = [
 
 ];
 
+var htmlQuote = "";
+
 function getRandomQuote (quotes) {
   var ind = Math.floor(Math.random() * 3);
   var chosenQuote = quotes[ind];
   return chosenQuote;
 }
 
-function printQuote() {
-  var quoteObj = getRandomQuote();
+function printQuote(htmlQuote) {
+  var quoteObj = getRandomQuote(quotes);
+  htmlQuote += '<p class="quote">' + quoteObj.quote + '</p>';
+  htmlQuote += '<p class="source">' + quoteObj.source + '<span class="citation">' + quoteObj.citation + '</span> <span class="year">' + quoteObj.year + '</span> </p>'
+  // event listener to respond to "Show another quote" button clicks
+  // when user clicks anywhere on the button, the "printQuote" function is called
+
+  document.getElementById('quote-box').innerHTML = htmlQuote;
+
 }
 
-
-
-
-
-
-
-
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
